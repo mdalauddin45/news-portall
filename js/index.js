38fd7d9d925e6e605163b4ff9cc68841a9f06fa4
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-09-03 07:37:45 +0600

    author information add

--- js/index.js
+++ js/index.js
@@ -8,9 +8,10 @@ const displayNav = navbar => {
     // console.log(navbar);
     const navContainer = document.getElementById("nav-bar")
     navbar.forEach(nav => {
+        const { category_name } = nav;
         const li = document.createElement('li');
         li.classList.add('nav-item')
-        li.innerHTML = `<a class="nav-link fs-3">${navbar.category_name}</a>
+        li.innerHTML = `<a class="nav-link fs-4 mb-4 mt-4">${category_name}</a>
         `;
         navContainer.appendChild(li);
     })
@@ -18,7 +19,7 @@ const displayNav = navbar => {
 loadNav();
 
 const loadAllNews = async () => {
-    const res = await fetch("https://openapi.programming-hero.com/api/news/2e78e5e0310c2e9adbb6efb1a263e745");
+    const res = await fetch("https://openapi.programming-hero.com/api/news/category/01");
     const data = await res.json();
     // return data;
     displayCardNews(data.data);
@@ -27,33 +28,45 @@ const loadAllNews = async () => {
 const displayCardNews = mynews => {
     console.log(mynews);
     const cardContainer = document.getElementById('card-container');
+    cardContainer.textContent = ' ';
     mynews.forEach(news => {
+        const { title, details, author, thumbnail_url } = news;
+        const { img, name, total_view } = author;
         const card = document.createElement('div');
         card.classList.add('row')
         card.innerHTML = `
     <div class="col-md-4 w-full">
-            <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+            <img src="${thumbnail_url}" class="img-fluid rounded-start" alt="...">
     </div>
     <div class="col-md-8">
         <div class="card-body">
-        <h5 id='cardFirstTitle' class="card-title">${news.title}</h5>
-        <p class="card-text">${news.details.length > 100 ? news.details.slice(0, 100) + '...' : news.details}</p>
-        </div>
+        <h5 id='cardFirstTitle' class="fs-2 ">${title}</h5>
+        <p class="card-text fs-5">${details.length > 200 ? details.slice(0, 200) + '...' : details
+            }</p >
+        </div >
         <div id="author-container" class="d-flex">
-        <div class="col d-flex" >
+        <div id='author-img' class="col d-flex" >
         
-        <img src="${news.author.img}"class="img-fluid rounded" style="height: 50px; alt="...">
+        <img src="${img}"class="img-fluid" style="height: 50px; alt="...">
         <div>
-         <h6 class="card-title mx-2">${news.author.name} </h6> </br>
-         <h6 class="mx-2">${news.author.published_date
-            }</h6 >
+         <h6 class="card-title mx-2">${name ? name : 'No data Avilable'} </h6> </br>
         </div >
        </div >
-       <div class="col">
-        ${news.total_view ? news.total_view : 0}
+       <div class="col"><i class="far fa-eye"></i>
+        ${total_view ? total_view : 0}
+  </div> 
+  <div class='me-4' id=''>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
   </div>
+ 
   <div class="col">
-  <button class="btn btn-primary" type="submit">Button</button>
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Details
+</button>
   </div>
         </div >
     </div >
@@ -64,23 +77,4 @@ const displayCardNews = mynews => {
 
 
 
-// const loadauthor = authors => {
-//     const authorContainer = document.getElementById('author-container');
-//     authors.forEach(author => {
-//         const div = document.createElement('div');
-//         div.classlist.add('row');
-//         div.innerHTML = `
-//     <div class="col">
-//           ${author.image_url}
-//     </div>
-//     <div class="col">
-//           ${author.total_view ? author.total_view : 0}
-//     </div>
-//     <div class="col">
-//          Column
-//     </div>
-//     `;
-//         authorContainer.appendChild(div);
-//     })
-// }
 loadAllNews();
